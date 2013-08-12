bff7758b3abbc5e0af5a876fd0dfc8c148a6932f
sabsab <zilzilya@gmail.com>
2013-08-12 16:21:22 +0400

    xls pager

---
+++ Classes/PHPExcel/Tools/Pager/XlsPager.php
@@ -0,0 +1,23 @@
+<?php
+require_once ("IExcelPager.php");
+/**
+ * Пейджер для XLS шаблонов
+ * @category  
+ * @package   
+ * @subpackage 
+ * @author: u.lebedev
+ * @date: 12.08.13
+ * @version    $Id: $
+ */
+
+class PHPExcel_Tools_Pager_XlsPager extends PHPExcel_Tools_Pager_Pager implements IExcelPager{
+    public function getSmoothedPageMap(){
+        $result = parent::getSmoothedPageMap();
+        $sheet = $this->getExcelSheet();
+        foreach ($result as $page){
+            $sheet->setBreak('A'.$page->getFinish(),PHPExcel_Worksheet::BREAK_ROW);
+        }
+        return $result;
+    }
+
+}
